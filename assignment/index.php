ef671ecd9fa2483521a2dfdd89b4d6cc6db468ad
sungjks <33453921+sungjks@users.noreply.github.com>
2017-11-07 19:38:52 +0700

    Add files via upload

---
+++ assignment/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Assignment</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="menu-top-width">
+		<div class="menu-top">
+			<div class="logo">
+				
+			</div>
+			<div class="menu-top-list">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Services</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
